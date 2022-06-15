943818b80028869accec732c72e25e4f7d62b23a
Christianib003 <christianib003@gmail.com>
2022-06-15 17:21:23 +0200

    fetch rockets form API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Rocket from './components/Rockets/Rocket';
 import Mission from './components/Missions/Missions';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
+import store from './Redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Rocket />} />
-      <Route path="/components/Missions/Missions" element={<Mission />} />
-      <Route path="/components/Profile/Profile" element={<Profile />} />
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rocket />} />
+        <Route path="/components/Missions/Missions" element={<Mission />} />
+        <Route path="/components/Profile/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
 );
