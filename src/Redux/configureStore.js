c4d7726c979a2224502c480d8b6ed4e47189e836
haroldmud <haroldmudosa@gmail.com>
2022-06-16 11:00:57 +0200

    fetch mission data

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rocketReducer from './Rockets/Rockets';
+import MissionsReducer from './Missions/missions';
 
 const reducer = combineReducers({
   rocketReducer,
+  MissionsReducer,
 });
 
 const store = createStore(
