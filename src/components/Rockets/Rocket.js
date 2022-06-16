1467979c6bcf01d14447b5b645c4d7eb93eb4686
Christianib003 <christianib003@gmail.com>
2022-06-16 16:18:57 +0200

    display rockets

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import './rockets.css';
 
-class Rocket extends Component {
-  state = {
-    Rocket: [],
-  }
+const Rocket = (props) => {
+  const {
+    rocketId, rocketName, rocketDescription, rocketImage, rocketReserved,
+  } = props;
+  return (
+    <div className="rocket-container" key={rocketId}>
+      <div>
+        <img src={rocketImage} alt="/" />
+      </div>
+      <div className="right">
+        <h2>
+          {rocketName}
+        </h2>
+        <p>
+          {rocketDescription}
+        </p>
+        <button type="button" className="btn" id={rocketReserved}>Reserve Rocket</button>
+      </div>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <h1>ELON MUSK-rock</h1>
-    );
-  }
-}
+Rocket.propTypes = {
+  rocketId: PropTypes.string.isRequired,
+  rocketName: PropTypes.string.isRequired,
+  rocketDescription: PropTypes.string.isRequired,
+  rocketImage: PropTypes.string.isRequired,
+  rocketReserved: PropTypes.bool.isRequired,
+};
 
 export default Rocket;
